97a54458f9ba0c8a5b4a20076e536ec1e3c55a8c
Gabriel Pereira <gabrieldasilvapp@gmail.com>
2024-02-01 16:07:19 -0300

    Adicionando aula 13

---
+++ aula13/aula13.js
@@ -0,0 +1,12 @@
+//                     01234567
+let umaString = 'Um texto';
+
+
+console.log(umaString.concat(' em um lindo dia. '));
+console.log(umaString + ' em um lindo dia. ');
+console.log(`${umaString} em um lindo dia. `)
+console.log(umaString.indexOf('texto'))
+console.log(umaString.lastIndexOf('m', 3))
+console.log(umaString.replace('Um', 'Outra'))
+array = umaString.split(' ')
+console.log(array)
